e66282f279cd7590055bbd6d1b68a15da6fa684f
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-05 22:18:47 +0500

    basic components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1>hello world</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
