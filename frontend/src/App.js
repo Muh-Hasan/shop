60e31b9e7ddab77f8a5528f4cfe3e9e595519396
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-08 20:27:47 +0500

    product component and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
-import { Container  }from "react-bootstrap"
-import Footer from './components/Footer';
-import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen'
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Container>
+        </main>
+        <Footer />
+      </>
+    </Router>
   );
 }
 
