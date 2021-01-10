56ba3e8cb612dc5d1220d2d5b68edb8348c501aa
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-10 15:49:15 +0500

    routing started

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
-const express = require("express")
+const express = require("express");
+const products = require("./data/products");
+const app = express();
 
-const app = express()
+app.get("/", (req, res) => {
+  res.send("API Runing");
+});
 
-app.listen(5000, console.log("server runing on port 5000"))
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.send(product);
+});
+
+app.listen(5000, console.log("server runing on port 5000"));
