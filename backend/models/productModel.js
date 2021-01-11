200a5d3cf89d5a98b6090d19d1a6caa91a304019
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-11 14:01:14 +0500

    order model done

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,20 +1,23 @@
 import mongoose from "mongoose";
-const reviewSchema = mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
+const reviewSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    rating:{
-        type: Number,
-        required: true,
+    rating: {
+      type: Number,
+      required: true,
     },
-    comment:{
-        type: String,
-        required: true,
-    }
-}, {
-    timestamps: true
-})
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const productrSchema = mongoose.Schema(
   {
