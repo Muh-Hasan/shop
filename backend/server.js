e33a3e309c347ae079707690ab05edbd79bd5381
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-11 17:20:45 +0500

    error middleware done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import colors from "colors"
-import productRoutes from './routes/productRoutes.js'
+import colors from "colors";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
+
 const app = express();
 
 connectDB();
@@ -11,10 +13,17 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("API Runing");
 });
-app.use("/api/products" , productRoutes)
+
+app.use("/api/products", productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(
   port,
-  console.log(`Server is running in ${process.env.NODE_ENV} on port ${port}`.yellow.bold)
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on port ${port}`.yellow.bold
+  )
 );
