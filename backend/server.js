b2541e9b45d8e96b73c4b5c0b462d8138f246ac2
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-11 14:38:45 +0500

    routes done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import colors from "colors"
+import productRoutes from './routes/productRoutes.js'
 dotenv.config();
 const app = express();
 
@@ -11,15 +11,7 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("API Runing");
 });
-
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.send(product);
-});
+app.use("/api/products" , productRoutes)
 
 const port = process.env.PORT || 5000;
 app.listen(
