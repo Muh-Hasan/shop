ba37b5c9ad0f2adbe6fe956772be749b94d7031e
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-12 20:17:57 +0500

    action created

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,26 +2,24 @@ import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
 import axios from "axios";
-import store from '../store'
-import { useSelector } from 'react-redux'
-import { productList } from '../reducres/productReducers'
+// import store from '../store'
+// import { useSelector } from 'react-redux'
+// import { productList } from '../reducres/productReducers'
 const HomeScreen = () => {
-  // const [products, setProducts] = useState([]);
-  const p = useSelector(async(state) => state.products)
-  console.log(p);
+  const [products, setProducts] = useState([]);
+  
   useEffect(() => {
-    store.dispatch(productList())
 
   },[]);
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        {/* {products.map((product) => (
+        {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
             <Product product={product} />
           </Col>
-        ))} */}
+        ))}
       </Row>
     </>
   );
