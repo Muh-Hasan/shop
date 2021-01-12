c6847a8a1aa0b38b2e9166a8246bccf94e38d285
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-12 21:21:15 +0500

    redux in homescreen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,14 +1,14 @@
-export const productListReducer = (state = { product: [] }, action) => {
+export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case "PRODUCT_LIST_REQUEST":
-      return { loading: true, products: [] };
-    
+      return { loading: true };
+
     case "PRODUCT_LIST_SUCCESS":
       return { loading: false, products: action.payload };
     case "PRODUCT_LIST_FAIL":
       return { loading: false, error: action.payload };
 
     default:
-      return state
+      return state;
   }
 };
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,26 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
-// import store from '../store'
-// import { useSelector } from 'react-redux'
-// import { productList } from '../reducres/productReducers'
-const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  
-  useEffect(() => {
+import { useSelector, useDispatch } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
-  },[]);
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+  useEffect(() => {
+    dispatch(listProducts);
+  }, [dispatch]);
+
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
