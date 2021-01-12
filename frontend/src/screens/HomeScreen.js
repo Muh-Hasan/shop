60eb95943a2aaf51507b41d789ecc1c00c331703
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-12 19:52:13 +0500

    resteup redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,25 +6,22 @@ import store from '../store'
 import { useSelector } from 'react-redux'
 import { productList } from '../reducres/productReducers'
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  const p = useSelector(state => state)
+  // const [products, setProducts] = useState([]);
+  const p = useSelector(async(state) => state.products)
   console.log(p);
   useEffect(() => {
-    fetchProducts()
-  }, []);
-  const fetchProducts = async () => {
-    await store.dispatch(productList())
-    
-  };
+    store.dispatch(productList())
+
+  },[]);
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => (
+        {/* {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
             <Product product={product} />
           </Col>
-        ))}
+        ))} */}
       </Row>
     </>
   );
