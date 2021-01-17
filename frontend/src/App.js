84837037c5f89efbab16dcf041614322632cc4fc
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-17 13:13:02 +0500

    reinstall react-router-dom v6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,9 +14,10 @@ function App() {
         <Header />
         <main className="py-3">
           <Container>
-              <Route path="/" component={<HomeScreen />} />
-              <Route path="/product/:id" component={<ProductScreen />} />
-              <Route path="/cart/:id?" component={<CartScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:id" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/cart/:id" element={<CartScreen />} />
           </Container>
         </main>
         <Footer />
