c5ac636e006e7359abae04c2b8800316cc8b254c
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-19 23:17:01 +0500

    code formatted

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,7 @@
 import express from "express";
-import {
-    authUser
-} from "../controllers/userController.js";
+import { authUser } from "../controllers/userController.js";
 const router = express.Router();
 
-
-router.post('/login', authUser)
-
+router.post("/login", authUser);
 
 export default router;
