500ffa5b77f27d0417f2b04c13967d89d1fe0192
Muh-Hasan <muh.hasan.git@gmail.com>
2021-01-19 23:15:36 +0500

    user auth done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
@@ -10,11 +11,15 @@ const app = express();
 
 connectDB();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API Runing");
 });
 
 app.use("/api/products", productRoutes);
+
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
